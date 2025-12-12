0bd0a9810848150e22b9f17b7ba19f7931525c8f
se-ejp <ckg24103@st.ckg.ac.jp>
2025-12-12 10:33:36 +0900

    future スレッド詳細+投稿返信機能の追加
            modified:   app/routers/posts.py　投稿・返信機能の追加
            modified:   app/routers/threads.py　スレッド詳細機能の追加
            modified:   app/templates/base.html	トップページへのリンク追加
            modified:   app/templates/index.html　投稿一覧に投稿番号（スレッド内）を追加
            new file:   app/templates/thread_detail.html	スレッド詳細画面

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routers import threads
 from app.routers import posts
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="BBS API",
@@ -8,7 +9,8 @@ app = FastAPI(
     version="0.1"
 )
 
-# ルーターを登録（まだ中身は空でOK）
+
 app.include_router(threads.router)
 app.include_router(posts.router)
 app.include_router(posts.threads_router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
